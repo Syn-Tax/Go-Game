14578f9629264171341dddd9329aa2025aca6577
Syn-Tax <twocap06@gmail.com>
2023-01-31 17:32:52 +0000

    fixed resize

--- Go-Game/Form1.cs
+++ Go-Game/Form1.cs
@@ -18,10 +18,11 @@ namespace Go_Game
             createGrid();
         }
 
-        protected virtual void OnResizeEnd(EventArgs e)
+        protected override void OnResize(EventArgs e)
         {
-            //renderGrid();
-            Console.WriteLine(this.Height);
+            base.OnResize(e);
+            renderGrid();
+            //Console.WriteLine(this.Height);
             //MessageBox.Show("you resized the window!!!");
         }
 
@@ -38,8 +39,16 @@ namespace Go_Game
 
         public void renderGrid() // The render grid function is responsible for loading the image
         {
+            // remove all picture boxes - NOTE: All things with tag "grid" will be re-rendered
+            foreach (Control control in this.Controls)
+            {
+                if (control is PictureBox && control.Tag.ToString() == "grid")
+                {
+                    this.Controls.Remove(control);
+                }
+            }
             // Writing the image to the form. Note that the render occurs from the exe (i.e. bin\Debug)
-            renderImage("../../assets/goBoard.png", 50, 50, this.Height, this.Height);
+            renderImage("../../assets/goBoard.png", (this.Width-(this.Height-75))/2, 30, this.Height-75, this.Height-75, "grid");
 
         }
 
@@ -50,12 +59,14 @@ namespace Go_Game
             result = MessageBox.Show("Go. Alexander Glen, Lewis Simmonds, Oscar Morris. (c) 2023", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        public PictureBox renderImage(string path, int x, int y, int w, int h) // The render image function handles writing any images.
+        public PictureBox renderImage(string path, int x, int y, int w, int h, string tag) // The render image function handles writing any images.
         {
             PictureBox pb = new PictureBox();
             pb.ImageLocation = path;
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
             pb.Location = new Point(x, y);
             pb.Size = new Size(w, h);
+            pb.Tag = tag;
             this.Controls.Add(pb);
             return pb;
         }
