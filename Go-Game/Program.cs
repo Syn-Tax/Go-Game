3b509c5f34d28cf0033d5b5196ea81e0546d740c
Nintails-TF <2455717@dundee.ac.uk>
2023-02-06 21:37:53 +0000

    Trying to call board from Program

--- Go-Game/Program.cs
+++ Go-Game/Program.cs
@@ -7,8 +7,9 @@ using System.Windows.Forms;
 
 namespace GoGame
 {
-    internal static class Program
+    internal class Program
     {
+        public GameBoard board = new GameBoard();
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,7 +18,7 @@ namespace GoGame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GameBoard());
+            Application.Run(board);
         }
     }
 }
