0f055408c60e2d26c614a918428dd4369f46c32d
Lewis Simmonds <simmondslewis5@gmail.com>
2023-02-09 21:00:27 +0000

    window now totally non-resizable

--- Go-Game/GameBoard.Designer.cs
+++ Go-Game/GameBoard.Designer.cs
@@ -178,6 +178,8 @@ namespace GoGame
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Controls.Add(this.mainMenuPanel);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
